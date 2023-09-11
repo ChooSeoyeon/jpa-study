2cb1d9d56e53cd32facec493634a3c56c8353e73
Choo <choo000407@naver.com>
2023-09-12 02:12:05 +0900

    fix: java 9 이상 지원 라이브러리 추가

--- jpa-basic/src/main/java/org/example/Main.java
+++ jpa-basic/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence");
+        EntityManager em = emf.createEntityManager();
+        em.close();
+        emf.close();
     }
 }
